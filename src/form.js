49e7418edfae7f1692b2cc18b46b584423b8f3ab
Aniket <Aniket8076206264@gmail.com>
2023-08-26 19:05:44 +0530

    init

--- src/form.js
+++ src/form.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
-  return (
+    const data ={name:"" ,email:"" , password:""};
+    const [inputData, setinputData] =useState(data)
+    function handleData(e){
+    setinputData({...inputData, [e.target.name]:e.target.value})
+  console.log(inputData)
+    }
+    return (
     <form className="container">
       <div className="header">
         <h1>Registration Form</h1>
       </div>
       <div>
-        <input type="text" placeholder="Enter your name" name="name"></input>
+        <input type="text" placeholder="Enter your name" name="name" value={inputData.name} onChange={handleData}></input>
        </div>
        <div>
-        <input type="text" placeholder="Enter your email" name="email"></input>
+        <input type="text" placeholder="Enter your email" name="email" value={inputData.email} onChange={handleData}></input>
        </div>
        <div>
-        <input type="text" placeholder="Enter your password" name="password"></input>
+        <input type="text" placeholder="Enter your password" name="password" value={inputData.password} onChange={handleData}></input>
        </div>
        <div>
         <button type="submit" >Submit</button>
